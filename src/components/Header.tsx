1227c5688115164d17cb0fdcffd683f99d8f42e7
Nataly-Costaa <natalynaty653@gmail.com>
2025-07-05 19:31:50 -0300

    :sparkles: feat: creating the header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="m-0 bg-green-600 h-16 flex items-center px-4">
+        <div>Logo</div>
+    </header>
+  )
+}
